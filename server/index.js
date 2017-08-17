e2baa698af32c99534629196af0c5a43b2fd751a
Owen Clarke <o.clarke@gmail.com>
2017-08-17 10:11:38 +0100

    Remove extra files we don't need in the firmware source copy

--- server/index.js
+++ server/index.js
@@ -47,15 +47,32 @@ app.post('/build', (req, res) => {
 		// Copy the base stencil.
 		yield new Promise((resolve, reject) => {
 			Exec('cp -rp qmk_firmware ' + TMP + key, (err, stdout, stderr) => {
-				if (err) return reject('Failed to initialize (1).');
+				if (err) return reject('Failed to initialize [cp qmk_firmware] [' + err + ']');
 				resolve();
 			});
 		});
 
+		// Remove extra files we don't need.
+		const rmfiles = ['docs',
+			'book.json',
+			'Dockerfile',
+			'Vagrantfile',
+			'tests',
+			'util',
+			'keyboards'];
+		for (var i = 0, len = rmfiles.length; i < len; i++) {
+			yield new Promise((resolve, reject) => {
+				Exec('rm -rf ' + TMP + key + '/' + rmfiles[i], (err, stdout, stderr) => {
+					if (err) return reject('Failed to initialize [rm ' + rmfiles[i] + '] [' + err + ']');
+					resolve();
+				});
+			});
+		}
+
 		// Copy the extra stencil.
 		yield new Promise((resolve, reject) => {
 			Exec('cp -rp qmk_firmware_extra/* ' + TMP + key, (err, stdout, stderr) => {
-				if (err) return reject('Failed to initialize (2).');
+				if (err) return reject('Failed to initialize [cp qmk_firmware_extra] [' + err + ']');
 				resolve();
 			});
 		});
@@ -65,7 +82,7 @@ app.post('/build', (req, res) => {
 			yield new Promise((resolve, reject) => {
 				const fileName = file.replace('qmk_firmware', TMP + key);
 				Fs.writeFile(fileName, files[file], err => {
-					if (err) return reject('Failed to initialize (3): ' + fileName);
+					if (err) return reject('Failed to initialize [cp ' + fileName + '] [' + err + ']');
 					resolve();
 				});
 			});
@@ -82,7 +99,7 @@ app.post('/build', (req, res) => {
 		// Read the hex file.
 		const hex = yield new Promise((resolve, reject) => {
 			Fs.readFile(TMP + key + '/kb_default.hex', 'utf8', (err, data) => {
-				if (err) return reject('Failed to read hex file.');
+				if (err) return reject('Failed to read hex file');
 				resolve(data);
 			});
 		});
